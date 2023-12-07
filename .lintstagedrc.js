d34f5663ab4ee02877e06e6471c661fa9c120032
Sam Moore <sam@paradigmpost.com>
2023-12-07 17:32:00 -0500

    chore: prettierignore to ignore files that cause it to error out while handling

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@
  * @type {import("lint-staged").Config}
  */
 module.exports = {
-	"*.{astro,css,ejs,js,json,jsx,md,mdx,mjs,ts,tsx,yaml,yml}": "bun run prettier --list-different",
+	'*': 'bun run prettier --list-different'
 }
